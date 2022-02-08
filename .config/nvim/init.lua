1f5eb5996633cdaa78f16b6e773475486bd2a86b
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2022-02-08 23:48:36 +0800

    feat: add init with config

---
+++ .config/nvim/init.lua
@@ -0,0 +1,28 @@
+local opt = vim.opt 
+
+--Displays
+
+opt.number = true -- Set number
+opt.relativenumber = true -- Set relative number
+opt.cursorline = true 
+opt.lazyredraw = true
+opt.hidden = true
+
+-- Identions
+
+opt.smartindent = true -- Insert indents automatically
+opt.shiftwidth = 4 -- Size of an indent
+opt.tabstop = 4 -- Number of spaces tabs count for
+
+-- Backups
+
+opt.swapfile = false
+
+-- Search
+
+opt.ignorecase = true -- Ignore case
+opt.wildignorecase = true -- Ignore case when completing file names and directories
+
+-- Colorscheme 
+
+opt.termguicolors = true
