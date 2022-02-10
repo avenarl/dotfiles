bdddd24ad4b63c79978c68409dee5502e623fb2c
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2022-02-10 21:57:42 +0800

    refactor(keymaps): change escape key in insert mode

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -43,7 +43,7 @@ vim.api.nvim_set_keymap('n', '<leader>1', ':lua terminal:open(1)<cr>', silent)
 vim.api.nvim_set_keymap('n', '<leader>2', ':lua terminal:open(2)<cr>', silent)
 vim.api.nvim_set_keymap('n', '<leader>3', ':lua terminal:open(3)<cr>', silent)
 map('t', '<ESC>', '<C-\\><C-n> <CR>') -- Escape mode
-map('i', 'jj', '<ESC>') -- Escape insert mode
+map('i', 'jk', '<ESC>') -- Escape insert mode
 
 -- Nvim Tree
 
