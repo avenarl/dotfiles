c2285fda0e8649a723141779ef7f0a0c7a23bc5c
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2022-02-11 23:02:46 +0800

    feat(plugins): install treesitter

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,6 +2,9 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
 	-- Packer can manage itself
-	use { 'wbthomason/packer.nvim', }
+	use 'wbthomason/packer.nvim'
+
+	-- Higlighting Syntax
+	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
 end)
