38efb0805e291721d4413817b2f7ef1be7b8852f
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2022-02-16 21:11:30 +0800

    feat(plugins): install colorscheme kanagawa.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,6 +3,7 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function()
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
+	use 'rebelot/kanagawa.nvim'
 
 	-- Higlighting Syntax
 	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
