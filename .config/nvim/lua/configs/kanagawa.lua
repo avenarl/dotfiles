a94398fd42fce595d13b97df873ef0ecb0dea71e
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2022-02-19 17:24:38 +0800

    refactor(plugins): kanagawa colors

--- .config/nvim/lua/configs/kanagawa.lua
+++ .config/nvim/lua/configs/kanagawa.lua
@@ -1,27 +1,29 @@
 require('kanagawa').setup({
     undercurl = true,           -- enable undercurls
     commentStyle = "italic",
-    functionStyle = "bold",
-    keywordStyle = "italic",
+    functionStyle = "NONE",
+    keywordStyle = "bold",
     statementStyle = "NONE",
     typeStyle = "NONE",
     variablebuiltinStyle = "italic",
     specialReturn = true,       -- special highlight for the return keyword
     specialException = true,    -- special highlight for exception handling keywords 
     transparent = false,        -- do not set background color
-    dimInactive = false,        -- dim inactive window `:h hl-NormalNC`
+    dimInactive = true,        -- dim inactive window `:h hl-NormalNC`
     colors = {
-		sumiInk1 = "#000000", -- background color
-		surimiOrange = "#E95420", -- constants, imports, booleans - orange
-		sakuraPink = "#DC322F", -- numbers - red
-		oniViolet = "#FCC82D", -- statements and keywords
-		springGreen = "#727169", -- string - dark grey
-		crystalBlue = "#934FFF", -- functions and titles - violet
-		springViolet2 = "#DC322F", -- brackets and punctuations
-		carpYellow = "#4D83C1", -- identifiers - orange
-		boatYellow2 = "#719e07", -- operators and regex
-		fujiWhite = "#719E07",
-		fujiGray = "#586E75"
+		sumiInk1 = "#16161D", -- background color
+		surimiOrange = "#934FFF", -- constants, imports, booleans
+		peachRed= "#934FFF", -- standout specials 2 (exception handling, return)
+		fujiWhite = "#934FFF", -- default foreground
+		sakuraPink = "#89E051", -- numbers
+		springViolet2 = "#89E051", -- brackets and punctuations
+		boatYellow2 = "#89E051", -- operators and regex
+		oniViolet = "#41B883", -- statements and keywords
+		waveRed = "#41B883", -- standout specials 1 (builtin variables)
+		springGreen = "#2B7489", -- string 
+		crystalBlue = "#FF3E00", -- functions and titles
+		carpYellow = "#00ADD8", -- identifiers 
+		fujiGray = "#586E75" -- comments
 	},
     overrides = {},
 })
