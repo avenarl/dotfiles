667da3e0810d06a31cfd386d86eb5e70be98a935
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2022-02-20 20:25:32 +0800

    refactor(plugins): remove curly braces and add comments

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,16 +3,18 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function()
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
+
+	-- Colorscheme
 	use 'rebelot/kanagawa.nvim'
 
 	-- Higlighting Syntax
 	use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
 
 	-- Hexcode color identifier
-	use { 'norcalli/nvim-colorizer.lua' }
+	use 'norcalli/nvim-colorizer.lua' 
 
 	-- Icons
-	use { 'kyazdani42/nvim-web-devicons' }
+	use 'kyazdani42/nvim-web-devicons'
 
 	-- Statusline requires Icons
 	use { 
