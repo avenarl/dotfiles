ffcdfa6a7b0f347b7784f398c995b4b7b34aeb62
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2022-02-21 19:01:31 +0800

    feat(plugins): add file explorer nvim tree

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -21,4 +21,8 @@ return require('packer').startup(function()
 		'nvim-lualine/lualine.nvim',
 		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
 	}
+
+	-- File explorer
+	use 'kyazdani42/nvim-tree.lua'
+
 end)
