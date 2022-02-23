bdffd4362406a8f5c923d2a9b7be22e7901030a9
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2022-02-23 23:09:23 +0800

    refactor(plugins): remove other lines

--- .config/nvim/lua/configs/cmp.lua
+++ .config/nvim/lua/configs/cmp.lua
@@ -1,27 +1,9 @@
 require('cmp').setup({
 	 snippet = {
-      -- REQUIRED - you must specify a snippet engine
-      expand = function(args)
-        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      end,
-	},
-
-	 mapping = {
-      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
-      ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-      ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-      ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-      ['<C-e>'] = cmp.mapping({
-        i = cmp.mapping.abort(),
-        c = cmp.mapping.close(),
-      }),
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    },
-
-	 sources = cmp.config.sources({
-      { name = 'vsnip' }, -- For vsnip users.
-    }, {
-      { name = 'buffer' },
-    })
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+		vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+		end,
+	}
 	
 }) 
