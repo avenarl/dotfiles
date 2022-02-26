0bd626b3e4e761d2a4d9da39ea1f81f6cfaf0403
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2022-02-27 00:23:02 +0800

    feat(plugins): add bufferline configuration

---
+++ .config/nvim/lua/configs/bufferline.lua
@@ -0,0 +1,11 @@
+vim.opt.termguicolors = true
+require('bufferline').setup {
+	offsets = {
+		{
+			filetype = "NvimTree",
+			text = "File Explorer",
+			highlight = "Directory",
+			text_align = "left"
+		}
+	}
+}
