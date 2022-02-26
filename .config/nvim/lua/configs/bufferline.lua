0fd751c6cd4b5ff5c5a47d442280c7c02c49819e
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2022-02-27 00:43:25 +0800

    feat(plugins): add bufferline configuration

--- .config/nvim/lua/configs/bufferline.lua
+++ .config/nvim/lua/configs/bufferline.lua
@@ -1,11 +1,12 @@
-vim.opt.termguicolors = true
-require('bufferline').setup {
-	offsets = {
-		{
-			filetype = "NvimTree",
-			text = "File Explorer",
-			highlight = "Directory",
-			text_align = "left"
-		}
-	}
+require("bufferline").setup{
+	 options = {
+      numbers = "none",
+      diagnostics = "nvim_lsp",
+      separator_style = "slant" or "padded_slant",
+      show_tab_indicators = true,
+      show_buffer_close_icons = false,
+      show_close_icon = false,
+    },
 }
+
+
