ab5aa01acbf604f26570972d15e786aace97bf7e
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-03-16 23:41:16 +0800

    feat(shell): add alias to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+set -g -x PATH /usr/local/bin $PATH
+
+# set alias for tracking of dotfiles
+alias config="git --git-dir=$HOME/.config --work-tree=$HOME"
