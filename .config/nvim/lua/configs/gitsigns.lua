1a295561202ae010fede492cdad3a14cafa77e80
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-03-17 15:59:35 +0800

    refactor(plugins): change git symbols

--- .config/nvim/lua/configs/gitsigns.lua
+++ .config/nvim/lua/configs/gitsigns.lua
@@ -1,26 +1,25 @@
 require('gitsigns').setup {
   signs = {
     add = { text = '++' },
-	change = { text = "|" },
-	delete = { text = "_" },
-	topdelete = { text = "‾" },
-	changedelete = { text = "~" },
-	untracked = { text = "┆" }, 
+    change = { text = "|" },
+    delete = { text = "_" },
+    topdelete = { text = "‾" },
+    changedelete = { text = "~" },
+    untracked = { text = "┆" }, 
   },
 	signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
   	numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
   	linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
   	word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff` watch_gitdir = {
-		interval = 1000,
+	interval = 1000,
     	follow_files = true,
-  	},
 	attach_to_untracked = true,
 	current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
   	current_line_blame_opts = {
 		virt_text = true,
 		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-    	delay = 1000,
-    	ignore_whitespace = false,
+    		delay = 1000,
+    		ignore_whitespace = false,
   	},
 	current_line_blame_formatter_opts = {
 		relative_time = false,
@@ -39,5 +38,6 @@ require('gitsigns').setup {
   },
 	yadm = {
 		enable = false,
-  },
+  }
+
 }
