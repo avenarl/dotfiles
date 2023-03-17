db5338c8248522d8d6e990bd48d1302428cf4e81
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-03-17 15:56:37 +0800

    refactor(plugins): change kanagawa setup

--- .config/nvim/lua/configs/kanagawa.lua
+++ .config/nvim/lua/configs/kanagawa.lua
@@ -1,30 +1,26 @@
 require('kanagawa').setup({
+    compile = false,
     undercurl = true,           -- enable undercurls
-    commentStyle = "italic",
-    functionStyle = "NONE",
-    keywordStyle = "bold",
-    statementStyle = "NONE",
-    typeStyle = "NONE",
+    commentStyle = { italic = true },
+    functionStyle = {},
+    keywordStyle = { italic = true },
+    statementStyle = { bold = true },
+    typeStyle = {},
     variablebuiltinStyle = "italic",
-    specialReturn = true,       -- special highlight for the return keyword
-    specialException = true,    -- special highlight for exception handling keywords 
-    transparent = false,        -- do not set background color
-    dimInactive = true,        -- dim inactive window `:h hl-NormalNC`
-    colors = {
-		sumiInk1 = "#16161D", -- background color
-		surimiOrange = "#934FFF", -- constants, imports, booleans
-		peachRed= "#934FFF", -- standout specials 2 (exception handling, return)
-		fujiWhite = "#934FFF", -- default foreground
-		sakuraPink = "#89E051", -- numbers
-		springViolet2 = "#FFFFFF", -- brackets and punctuations
-		boatYellow2 = "#89E051", -- operators and regex
-		oniViolet = "#41B883", -- statements and keywords
-		waveRed = "#41B883", -- standout specials 1 (builtin variables)
-		springGreen = "#2B7489", -- string 
-		crystalBlue = "#FF3E00", -- functions and titles
-		carpYellow = "#00ADD8", -- identifiers 
-		fujiGray = "#586E75" -- comments
-	},
-    overrides = {},
+    transparent = false,
+    dimInactive = false,
+    terminalCOlors = true,
+    colors = {                   -- add/modify theme and palette colors
+	palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+    },
+    overrides = function(colors) -- add/modify highlights
+        return {}
+    end,
+    theme = "wave",              -- Load "wave" theme when 'background' option is not set
+    background = {               -- map the value of 'background' option to a theme
+        dark = "wave",           -- try "dragon" !
+        light = "lotus"
+    },
 })
 
