4b6a46d3f2b45e91a70509e216a6451a0450b269
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-03-19 21:30:34 +0800

    feat(shell): set alias of fish configuration directory

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,4 +7,8 @@ set -g -x PATH /usr/local/bin $PATH
 # set alias for tracking of dotfiles
 alias dot="git --git-dir=$HOME/.config --work-tree=$HOME"
 
-alias vim "nvim"
+# honor for vim
+alias vim "nvim" 
+
+# when updating the config of fish
+alias fishy "nvim ~/.config/fish/config.fish"
