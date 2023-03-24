84c75059e099b98e08e6395d78e9d03e7c8f616b
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-03-24 20:50:18 +0800

    feat(plugins): set configuration for eslint

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,2 +1,28 @@
 local lspconfig = require('lspconfig')
-lspconfig.tsserver.setup{}
+
+-- TypeScript language server configuration
+lspconfig.tsserver.setup{} 
+
+-- ESLint configuration
+lspconfig.efm.setup {
+    init_options = {documentFormatting = true},
+    filetypes = {"javascript", "javascriptreact", "typescript", "typescriptreact"},
+    settings = {
+        rootMarkers = {".git/"},
+        languages = {
+            javascript = {
+                {lintCommand = "eslint -f unix --stdin --stdin-filename ${INPUT}", lintStdin = true, lintFormats = {"%f:%l:%c: %m"}}
+            },
+            javascriptreact = {
+                {lintCommand = "eslint -f unix --stdin --stdin-filename ${INPUT}", lintStdin = true, lintFormats = {"%f:%l:%c: %m"}}
+            },
+            typescript = {
+                {lintCommand = "eslint -f unix --stdin --stdin-filename ${INPUT}", lintStdin = true, lintFormats = {"%f:%l:%c: %m"}}
+            },
+            typescriptreact = {
+                {lintCommand = "eslint -f unix --stdin --stdin-filename ${INPUT}", lintStdin = true, lintFormats = {"%f:%l:%c: %m"}}
+            },
+        },
+    },
+}
+
