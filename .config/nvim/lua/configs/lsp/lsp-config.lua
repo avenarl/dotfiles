0d6663de2150969e027ea8373fa47e8847a602a6
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-03-25 21:19:45 +0800

    feat(plugins): import plugins in lsp configuration

---
+++ .config/nvim/lua/configs/lsp/lsp-config.lua
@@ -0,0 +1,24 @@
+-- import lspconfig plugin safely
+local lspconfig_status, lspconfig = pcall(require, 'lspconfig')
+if not lspconfig_status then
+  return
+end
+
+-- import cmp-nvim-lsp plugin safely
+local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
+if not cmp_nvim_lsp_status then
+  return
+end
+
+-- import typescript plugin safely
+local typescript_setup, typescript = pcall(require, 'typescript')
+if not typescript_setup then
+  return
+end
+
+local keymap = vim.keymap -- for conciseness
+
+-- enable keybinds only for when lsp server available
+local on_attach = function(client, bufnr)
+  -- keybind options
+  local opts = { noremap = true, silent = true, buffer = bufnr }
