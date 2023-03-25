aa269be861b0594e4573c3e86f723d482c759290
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-03-25 18:41:47 +0800

    feat(plugins): add setup for mason and mason-lsp

--- .config/nvim/lua/configs/lsp/mason.lua
+++ .config/nvim/lua/configs/lsp/mason.lua
@@ -9,3 +9,20 @@ local mason_lspconfig_status, mason_lspconfig = pcall(require, 'mason-lspconfig'
 if not mason_lspconfig_status then
   return
 end
+
+-- enable mason
+mason.setup()
+
+mason_lspconfig.setup({
+  -- list of servers for mason to install
+  ensure_installed = {
+    "tsserver",
+    "html",
+    "cssls",
+    "tailwindcss",
+    "lua_ls",
+    "emmet_ls",
+  },
+  -- auto-install configured servers (with lspconfig)
+  automatic_installation = true, -- not the same as ensure_installed
+})
