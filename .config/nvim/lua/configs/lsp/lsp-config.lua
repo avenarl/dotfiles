6a327285f3992d39c87fd50a1d08dfd265d9e3ec
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-03-25 22:07:00 +0800

    chore(plugins): subsitute quotes to single

--- .config/nvim/lua/configs/lsp/lsp-config.lua
+++ .config/nvim/lua/configs/lsp/lsp-config.lua
@@ -49,7 +49,7 @@ end
 local capabilities = cmp_nvim_lsp.default_capabilities()
 
 -- configure html server
-lspconfig["html"].setup({
+lspconfig['html'].setup({
   capabilities = capabilities,
   on_attach = on_attach,
 })
@@ -63,46 +63,46 @@ typescript.setup({
 })
 
 -- configure css server
-lspconfig["cssls"].setup({
+lspconfig['cssls'].setup({
   capabilities = capabilities,
   on_attach = on_attach,
 })
 
 -- configure tailwindcss server
-lspconfig["tailwindcss"].setup({
+lspconfig['tailwindcss'].setup({
   capabilities = capabilities,
   on_attach = on_attach,
 })
 
 -- configure emmet language server
-lspconfig["emmet_ls"].setup({
+lspconfig['emmet_ls'].setup({
   capabilities = capabilities,
   on_attach = on_attach,
-  filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
+  filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less', 'svelte' },
 })
 
 --configure angular language server
-lspconfig["angularls"].setup({
+lspconfig['angularls'].setup({
   capabilities = capabilities,
   on_attach = on_attach,
-  filetypes = { "typescript", "html", "typescriptreact", "typescript.tsx" },
+  filetypes = { 'typescript', 'html', 'typescriptreact', 'typescript.tsx' },
 })
 
 -- configure lua server (with special settings)
-lspconfig["lua_ls"].setup({
+lspconfig['lua_ls'].setup({
   capabilities = capabilities,
   on_attach = on_attach,
   settings = { -- custom settings for lua
     Lua = {
-      -- make the language server recognize "vim" global
+      -- make the language server recognize 'vim' global
       diagnostics = {
-        globals = { "vim" },
+        globals = { 'vim' },
       },
       workspace = {
         -- make language server aware of runtime files
         library = {
-          [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.stdpath("config") .. "/lua"] = true,
+          [vim.fn.expand('$VIMRUNTIME/lua')] = true,
+          [vim.fn.stdpath('config') .. '/lua'] = true,
         },
       },
     },
