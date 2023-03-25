74009a4e8fe82c71d4264d6f1793d5723bc6c78c
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-03-25 17:30:01 +0800

    refactor(plugins): re-install snippets

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -33,6 +33,11 @@ return require('packer').startup(function()
 	use 'hrsh7th/cmp-buffer' -- source for text in buffer
 	use 'hrsh7th/cmp-path' -- source for file system paths
 
+	-- snippets
+  	use('L3MON4D3/LuaSnip') -- snippet engine
+  	use('saadparwaiz1/cmp_luasnip') -- for autocompletion
+  	use('rafamadriz/friendly-snippets') -- useful snippets
+
 	-- Brackets  
 	use 'windwp/nvim-autopairs'
 	
