aa63d7a0a8f7047af1c210c19f4f8f9368c819ad
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-05-03 23:00:21 +0800

    :shirt: refactor(config): change path

--- .config/nvim/ftplugin/java.lua
+++ .config/nvim/ftplugin/java.lua
@@ -1,24 +1,32 @@
+local M = {}
+
+local home = os.getenv("HOME")
+local jdtls_installation_path = home .. ".config/jdtls"
+local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ":p:h:t")
+local workspace_dir = home .. "Projects/" .. project_name
+
 local config = {
 	cmd = {
-		-- command configuration
-	},
-	settings = {
-		-- settings configuration
-	},
-	init_options = {
-		bundles = {
-			vim.fn.glob(
-				"~/.config/nvim/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-0.45.0.jar",
-				1
-			),
-		},
+		"java",
+		"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+		"-Dosgi.bundles.defaultStartLevel=4",
+		"-Declipse.product=org.eclipse.jdt.ls.core.product",
+		"-Dlog.protocol=true",
+		"-Dlog.level=ALL",
+		"-Xmx1g",
+		"-javaagent:" .. home .. ".local/share/nvim/mason/packages/jdtls/lombok.jar",
+		"--add-modules=ALL-SYSTEM",
+		"--add-opens",
+		"java.base/java.util=ALL-UNNAMED",
+		"--add-opens",
+		"java.base/java.lang=ALL-UNNAMED",
+		"-jar",
+		jdtls_installation_path .. "/plugins/org.eclipse.equinox.launcher_1.6.400.v20210924-0641.jar",
+		"-configuration",
+		jdtls_installation_path .. "/config_linux",
+		"-data",
+		workspace_dir,
 	},
 }
 
-config["on_attach"] = function(client, bufnr)
-	require("jdtls").setup_dap({ hotcodereplace = "auto" })
-end
-
--- This starts a new client & server,
--- or attaches to an existing client & server depending on the `root_dir`.
-require("jdtls").start_or_attach(config)
+return M
