1cce80f40c291295554327d4711e707411c2ff22
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-05-05 13:27:18 +0800

    :up: update(config): update mappings for java debug

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -77,11 +77,39 @@ map("n", "<C-l>", ":<C-U>TmuxNavigateRight<cr>") -- Right tmux navigation
 -- Change double quote to single quote
 vim.api.nvim_set_keymap("n", "<leader>s", [[:%s/'\([^']*\)"/"\1"/g<CR>]], { noremap = true })
 
+-- Java Development
+vim.g.mapleader = " "
+
+-- key_mapping --
+local key_map = function(mode, key, result)
+	vim.api.nvim_set_keymap(mode, key, result, { noremap = true, silent = true })
+end
+
+-- run debug
+function get_test_runner(test_name, debug)
+	if debug then
+		return 'mvn test -Dmaven.surefire.debug -Dtest="' .. test_name .. '"'
+	end
+	return 'mvn test -Dtest="' .. test_name .. '"'
+end
+
+function run_java_test_method(debug)
+	local utils = require("utils")
+	local method_name = utils.get_current_full_method_name("\\#")
+	vim.cmd("term " .. get_test_runner(method_name, debug))
+end
+
+function run_java_test_class(debug)
+	local utils = require("utils")
+	local class_name = utils.get_current_full_class_name()
+	vim.cmd("term " .. get_test_runner(class_name, debug))
+end
+
 function get_spring_boot_runner(profile, debug)
 	local debug_param = ""
 	if debug then
 		debug_param =
-			' -Dspring-boot.run.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"'
+			' -Dspring-boot.run.jvmArguments="-Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005" '
 	end
 
 	local profile_param = ""
@@ -93,12 +121,59 @@ function get_spring_boot_runner(profile, debug)
 end
 
 function run_spring_boot(debug)
-	vim.cmd("term " .. get_spring_boot_runner("local", debug))
+	vim.cmd("term " .. get_spring_boot_runner(method_name, debug))
 end
 
+vim.keymap.set("n", "<leader>tm", function()
+	run_java_test_method()
+end)
+vim.keymap.set("n", "<leader>TM", function()
+	run_java_test_method(true)
+end)
+vim.keymap.set("n", "<leader>tc", function()
+	run_java_test_class()
+end)
+vim.keymap.set("n", "<leader>TC", function()
+	run_java_test_class(true)
+end)
 vim.keymap.set("n", "<c-r>", function()
 	run_spring_boot()
 end)
-vim.keymap.set("n", "<a-r>", function()
+vim.keymap.set("n", "<c-R>", function()
 	run_spring_boot(true)
 end)
+
+-- setup debug
+key_map("n", "<leader>b", ':lua require"dap".toggle_breakpoint()<CR>')
+key_map("n", "<leader>B", ':lua require"dap".set_breakpoint(vim.fn.input("Condition: "))<CR>')
+key_map("n", "<leader>bl", ':lua require"dap".set_breakpoint(nil, nil, vim.fn.input("Log: "))<CR>')
+key_map("n", "<leader>dr", ':lua require"dap".repl.open()<CR>')
+
+-- view informations in debug
+function show_dap_centered_scopes()
+	local widgets = require("dap.ui.widgets")
+	widgets.centered_float(widgets.scopes)
+end
+key_map("n", "gs", ":lua show_dap_centered_scopes()<CR>")
+
+-- move in debug
+key_map("n", "<F5>", ':lua require"dap".continue()<CR>')
+key_map("n", "<F8>", ':lua require"dap".step_over()<CR>')
+key_map("n", "<F7>", ':lua require"dap".step_into()<CR>')
+key_map("n", "<S-F8>", ':lua require"dap".step_out()<CR>')
+
+function attach_to_debug()
+	local dap = require("dap")
+	dap.configurations.java = {
+		{
+			type = "java",
+			request = "attach",
+			name = "Attach to the process",
+			hostName = "localhost",
+			port = "5005",
+		},
+	}
+	dap.continue()
+end
+
+key_map("n", "<leader>da", ":lua attach_to_debug()<CR>")
