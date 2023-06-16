2761a24f5dd03258451ce7d825426a681c9c9956
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-06-16 23:24:31 +0800

    :up: config(autocommands): test format

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -1,3 +1,13 @@
+vim.api.nvim_create_autocmd({ "FileType " }, {
+	pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
+	callback = function()
+		vim.cmd([[
+			nnoremap <silent> <buffer> q :close<CR>
+			set nobuflisted
+		]])
+	end,
+})
+
 vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
 	callback = function()
