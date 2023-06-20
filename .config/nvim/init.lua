695f1c0bbd8da11052fedd894b69603d402bcc36
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-06-20 22:00:03 +0800

    :shirt: refactor(config): modify options vim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,7 @@
 require "plugins"
 require "utils"
 require "mappings"
+require "options"
 require "configs.treesitter"
 require "configs.kanagawa"
 require "configs.lualine"
@@ -21,55 +22,5 @@ require "configs.lsp.nvim-lsp-installer"
 require "configs.prettier"
 require "configs.whitespace"
 require "configs.trim"
-require "autocommands"
 require("nvim-tree").setup {}
 --require("telescope").setup({})
-
-local opt = vim.opt
-
-vim.cmd "colorscheme gruvbox"
-
--- Disable mouse
-opt.mouse = ""
-
--- Windows
-opt.splitbelow = true -- force all horizontal splits to go below current window
-opt.splitright = true -- force all vertical splits to go to the right of current window
-
---Displays
-
-opt.number = true -- Set number
-opt.relativenumber = true -- Set relative number
-opt.cursorline = true
-opt.lazyredraw = true
-opt.hidden = true
-opt.linebreak = true
-
--- Identions
-opt.expandtab = true -- Use softtabstop spaces instead of tab characters for indentation
-opt.shiftwidth = 2 -- Indent by 4 spaces when using >>, <<, == etc.
-opt.softtabstop = 2 -- Indent by 4 spaces when pressing <TAB>
-
-opt.autoindent = true -- Keep indentation from previous line
-opt.smartindent = true -- Automatically inserts indentation in some cases
-opt.cindent = true -- Like smartindent, but stricter and more customisable
-
--- Backups
-
-opt.swapfile = false
-
--- Apperance
-opt.termguicolors = true
-opt.background = "dark"
-opt.signcolumn = "yes"
-opt.colorcolumn = "125"
-
--- Search
-
-opt.ignorecase = true -- Ignore case
-opt.wildignorecase = true -- Ignore case when completing file names and directories
-opt.smartcase = true -- Smart case
-
-opt.clipboard:append "unnamedplus"
-opt.shortmess:append "c" -- Hide all the completion messages, e.g. "-- XXX completion (YYY)", "match 1 of 2", "The only match", "Pattern not found"
-opt.formatoptions:remove { "c", "r", "o" } -- This is a sequence of letters which describes how automatic formatting is to be done
---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,46 @@
+vim.cmd "colorscheme gruvbox"
+
+-- Disable mouse
+vim.opt.mouse = ""
+
+-- Windows
+vim.opt.splitbelow = true -- force all horizontal splits to go below current window
+vim.opt.splitright = true -- force all vertical splits to go to the right of current window
+
+--Displays
+
+vim.opt.number = true -- Set number
+vim.opt.relativenumber = true -- Set relative number
+vim.opt.cursorline = true
+vim.opt.lazyredraw = true
+vim.opt.hidden = true
+vim.opt.linebreak = true
+
+-- Identions
+vim.opt.expandtab = true -- Use softtabstop spaces instead of tab characters for indentation
+vim.opt.shiftwidth = 2 -- Indent by 4 spaces when using >>, <<, == etc.
+vim.opt.softtabstop = 2 -- Indent by 4 spaces when pressing <TAB>
+
+vim.opt.autoindent = true -- Keep indentation from previous line
+vim.opt.smartindent = true -- Automatically inserts indentation in some cases
+vim.opt.cindent = true -- Like smartindent, but stricter and more customisable
+
+-- Backups
+
+vim.opt.swapfile = false
+
+-- Apperance
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "125"
+
+-- Search
+
+vim.opt.ignorecase = true -- Ignore case
+vim.opt.wildignorecase = true -- Ignore case when completing file names and directories
+vim.opt.smartcase = true -- Smart case
+
+vim.opt.clipboard:append "unnamedplus"
+vim.opt.shortmess:append "c" -- Hide all the completion messages, e.g. "-- XXX completion (YYY)", "match 1 of 2", "The only match", "Pattern not found"
+vim.opt.formatoptions:remove { "c", "r", "o" } -- This is a sequence of letters which describes how automatic formatting is to be done
