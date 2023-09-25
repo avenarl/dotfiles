b564d17842e33e71c17287b20e80221ee1209fc7
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-09-25 10:34:29 +0800

    :heavy_plus_sign: config(folding): add ufo

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -21,6 +21,10 @@ return require("packer").startup(function(use)
       require("trim").setup {}
     end,
   }
+  use {
+    "mcauley-penney/tidy.nvim",
+    config = true,
+  }
 
   -- Higlighting Syntax
   use { "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" }
@@ -74,6 +78,7 @@ return require("packer").startup(function(use)
   -- formatting & linting
   use "jose-elias-alvarez/null-ls.nvim" -- configure formatters & linters
   use "jayp0521/mason-null-ls.nvim" -- bridges gap b/w mason & null-ls
+  use { "kevinhwang91/nvim-ufo", requires = "kevinhwang91/promise-async" }
 
   -- Buffer
   use {
