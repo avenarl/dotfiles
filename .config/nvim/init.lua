9ad5e6922e03ef66ef8c479ee6d61e3d0f263e5b
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-09-29 21:35:12 +0800

    :up: update(init): remove ufo folding

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -21,7 +21,6 @@ require "configs.nvim-dap"
 require "configs.lsp.nvim-lsp-installer"
 require "configs.prettier"
 require "configs.whitespace"
-require "configs.ufo"
 require "configs.trim"
 require("nvim-tree").setup {}
 --require("telescope").setup({})
