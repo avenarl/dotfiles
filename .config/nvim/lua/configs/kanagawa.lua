17e71899e0381773610b7cf3f67f443819b2ffd2
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-10-03 22:18:40 +0800

    :up: update(configs): modify colorscheme

--- .config/nvim/lua/configs/kanagawa.lua
+++ .config/nvim/lua/configs/kanagawa.lua
@@ -1,25 +1,25 @@
-require("kanagawa").setup({
-	compile = false,
-	undercurl = true, -- enable undercurls
-	commentStyle = { italic = true },
-	functionStyle = { bold = false },
-	keywordStyle = { bold = false },
-	statementStyle = { bold = false },
-	typeStyle = {},
-	variablebuiltinStyle = "italic",
-	transparent = false,
-	dimInactive = false,
-	terminalColors = true,
-	colors = { -- add/modify theme and palette colors
-		palette = {},
-		theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-	},
-	overrides = function(colors) -- add/modify highlights
-		return {}
-	end,
-	theme = "wave", -- Load "wave" theme when 'background' option is not set
-	background = { -- map the value of 'background' option to a theme
-		dark = "wave", -- try "dragon" !
-		light = "lotus",
-	},
-})
+require("kanagawa").setup {
+  compile = false,
+  undercurl = true, -- enable undercurls
+  commentStyle = { italic = true },
+  functionStyle = { bold = false },
+  keywordStyle = { bold = false },
+  statementStyle = { bold = false },
+  typeStyle = {},
+  variablebuiltinStyle = "italic",
+  transparent = false,
+  dimInactive = false,
+  terminalColors = true,
+  colors = { -- add/modify theme and palette colors
+    palette = {},
+    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+  },
+  overrides = function(colors) -- add/modify highlights
+    return {}
+  end,
+  theme = "wave", -- Load "wave" theme when 'background' option is not set
+  background = { -- map the value of 'background' option to a theme
+    dark = "wave", -- try "dragon" !
+    light = "lotus",
+  },
+}
