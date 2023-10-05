dfed132859dd031dccbd165096ea7b9b925c341d
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2023-10-05 22:04:08 +0800

    :up: update(configs): modify options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -19,7 +19,8 @@ vim.opt.linebreak = true
 -- Identions
 vim.opt.expandtab = true -- Use softtabstop spaces instead of tab characters for indentation
 vim.opt.shiftwidth = 2 -- Indent by 4 spaces when using >>, <<, == etc.
-vim.opt.softtabstop = 2 -- Indent by 4 spaces when pressing <TAB>
+vim.opt.tabstop = 2 -- 1 tab == 2 spaces
+vim.opt.smartindent = true -- AUtoindent new lines
 
 vim.opt.autoindent = true -- Keep indentation from previous line
 vim.opt.smartindent = true -- Automatically inserts indentation in some cases
@@ -44,3 +45,11 @@ vim.opt.smartcase = true -- Smart case
 vim.opt.clipboard:append "unnamedplus"
 vim.opt.shortmess:append "c" -- Hide all the completion messages, e.g. "-- XXX completion (YYY)", "match 1 of 2", "The only match", "Pattern not found"
 vim.opt.formatoptions:remove { "c", "r", "o" } -- This is a sequence of letters which describes how automatic formatting is to be done
+
+function _G.strip_trailing_whitespace()
+  vim.cmd [[%s/\s\+$//e]]
+end
+
+vim.cmd [[
+  autocmd BufWritePre * lua strip_trailing_whitespace()
+]]
