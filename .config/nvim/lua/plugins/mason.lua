4ce80f37cecb9c6c19f48c4440a458e1dec2649a
Raven Rodrigo <ravencarlostrodrigo@gmail.com>
2024-06-19 23:06:24 +0800

    :up: update(config): remove lsp servers

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -3,18 +3,7 @@ return {
   opts = function(_, opts)
     ---@diagnostic disable-next-line: missing-parameter
     opts.ensure_installed = vim.list_extend(opts.ensure_installed, {
-      "bashls",
-      "cssls",
-      "html",
-      "gradle_ls",
-      "groovyls",
-      "lua_ls",
       "jdtls",
-      "jsonls",
-      "dockerls",
-      "tsserver",
-      "rust_analyzer",
-      "yamlls",
     })
   end,
 }
